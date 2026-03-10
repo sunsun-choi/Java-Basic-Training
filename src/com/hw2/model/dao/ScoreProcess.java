62ecaff7b7d3635793c4c034af766e52e99ca65d
sunsun-choi <tldpsldk@gmail.com>
2026-03-10 14:15:50 +0900

    입출력예제2 : avg = sum/3.0 (int/int는 결과값이 int)

--- src/com/hw2/model/dao/ScoreProcess.java
+++ src/com/hw2/model/dao/ScoreProcess.java
@@ -1,10 +1,13 @@
 package com.hw2.model.dao;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class ScoreProcess {//클래스영역시작
 
@@ -41,6 +44,7 @@ public class ScoreProcess {//클래스영역시작
 				// "수학 : "  >>  int math
 				// sum(int)과 avg(double)를 도출 한 후 
 				// 위의 데이터들 순서대로 파일에 출력
+				
 				System.out.println("======다음의 성적들을 입력하시오======");
 				System.out.print("국어 : ");
 				int kor = sc.nextInt();
@@ -53,14 +57,13 @@ public class ScoreProcess {//클래스영역시작
 				sc.nextLine();
 				
 				int sum = (kor + eng + math);
-				double avg = (sum / 3);  //???통로가 1byte잖아 이건 8byte고...
-				String strAvg = avg+""; 
-				StringTokenizer token = new StringTokenizer(strAvg, ".");
+				double avg = (sum / 3.0);  //   **int와 int의 연산에서는 소수점이 자동으로 버려진다**
 				
-				
+				dos.write(kor);
+				dos.write(eng);
+				dos.write(math);
 				dos.write(sum);
-				dos.write(token.nextToken());
-				
+				dos.writeDouble(avg);
 				
 				num++;
 				// “num번째 학생 정보 기록” 출력
@@ -68,7 +71,7 @@ public class ScoreProcess {//클래스영역시작
 				// n을 입력했다면 “score.dat 파일에 저장 완료”라는 출력문구가 나오고 
 				// 반복문 빠져나감
 				System.out.println(num + "번째 학생 정보 기록");
-				System.out.print("계속 저장하시겠습니까? (y/n)");
+				System.out.print("계속 저장하시겠습니까? (y/n) :");
 				String istrue = sc.nextLine();
 				if(istrue.equals("n")||istrue.equals("N")) {   
 					System.out.println("score.dat 파일에 저장 완료");
@@ -96,20 +99,40 @@ public class ScoreProcess {//클래스영역시작
 		// 몇 명의 학생인지 or 반복문이 실행된 횟수(둘 다 같은 개념)
 		// try with resource 구문을 이용하여 (DataInputStream, FileInputStream)
 		// “score.dat” 파일과 연결통로를 만들어 주고
+		try(DataInputStream dis = new DataInputStream( new FileInputStream("score.dat")) ){
+			
+			
 		while(true){
 		// 데이터에 입력된 순서대로 readXXX() 메소드를 통해 읽어온 값을
 		// 위의 (kor, eng, mat, sum, avg)에 담고 출력
 		// 그리고 avgS에 해당 학생의 avg값 추가
-		count++;
+			kor = dis.read();
+			eng = dis.read();
+			mat = dis.read();
+			sum = dis.read();
+			avg = dis.readDouble();
+			avgS += avg;
+			count++;
 		// "count번째 학생" 출력
+			System.out.println(count + "번째 학생");
 		// "국	영	수	총점	평균" 출력
+			System.out.println("국	영	수	총점	평균");
 		// 위의 변수에 담아뒀던 내용 출력
+			System.out.println(kor+"	"+eng+"	"+mat+"	"+sum+"	"+avg);
 		}
+		
 		// EOFException이 발생하면,
 		// "읽은 횟수     전체 평균" 출력
 		//  데이터를 읽은 횟수(count),  전체 평균(avgS)에
 		//  대한 정보가 위의 구문 아래에 출력되게 함
 		// “score.dat 파일 읽기 완료!” 문구 출력
+		}catch(EOFException e) { 
+			System.out.println("읽은 횟수     전체 평균");
+			System.out.println(count+"       "+avgS/count);
+		}catch(FileNotFoundException e) {e.printStackTrace();
+		}catch(IOException e) {e.printStackTrace();}
+		
+		System.out.println("score.dat 파일 읽기 완료!");
 	}
 	
 	
--- src/com/hw2/run/Run.java
+++ src/com/hw2/run/Run.java
@@ -1,5 +1,7 @@
 package com.hw2.run;
 
+import com.hw2.model.dao.ScoreProcess;
+
 public class Run {
 	
 	
@@ -7,6 +9,9 @@ public class Run {
 	// ScoreProcess 클래스 객체 생성	후 각 메소드를 순차적으로 실행
 	public static void main(String[] args) {
 		
+		ScoreProcess sp = new ScoreProcess();
+		sp.scoreSave();
+		sp.scoreRead();
 
 	}
 
