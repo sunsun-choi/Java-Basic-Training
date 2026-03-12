d2c9715382bc3bb5c1f89bec9f9943e51e2e1068
tldps <tldps@172.30.1.38>
2026-03-12 22:35:46 +0900

    수정후 : 1줄만 수정하여 디버깅하기

--- src/kr/co/programmers/PCCE_06_Solution.java
+++ src/kr/co/programmers/PCCE_06_Solution.java
@@ -6,7 +6,7 @@ public class PCCE_06_Solution {
 	    public int solution(int storage, int usage, int[] change) {
 	        int total_usage = 0;
 	        for(int i=0; i<change.length; i++){
-	            usage = total_usage * change[i] / 100;
+	        	usage = usage * (100 + change[i]) / 100;
 	            total_usage += usage;
 	            if(total_usage > storage){
 	                return i;
