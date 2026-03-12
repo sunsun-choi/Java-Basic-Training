6a2b92963a24f4b7947a46b87dad5575f641159b
tldps <tldps@172.30.1.38>
2026-03-12 22:34:29 +0900

    수정전

---
+++ src/kr/co/programmers/PCCE_06_Solution.java
@@ -0,0 +1,19 @@
+package kr.co.programmers;
+
+public class PCCE_06_Solution {
+	
+	
+	    public int solution(int storage, int usage, int[] change) {
+	        int total_usage = 0;
+	        for(int i=0; i<change.length; i++){
+	            usage = total_usage * change[i] / 100;
+	            total_usage += usage;
+	            if(total_usage > storage){
+	                return i;
+	            }
+	        }
+	        return -1;
+	    }
+	
+
+}
